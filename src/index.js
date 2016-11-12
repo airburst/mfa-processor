06bb7840596a0d566f3995b458870a9899d6725d
airburst <mark@fairhursts.net>
2016-11-12 12:26:44 +0000

    Changes to logging

--- src/index.js
+++ src/index.js
@@ -7,36 +7,38 @@ import CouchService from './couchService'
 import { hexEncode, hexDecode } from './hexEncoder'
 
 // Set up logging transports
-var logger = new (winston.Logger)({
+const logger = new (winston.Logger)({
     transports: [
         new (winston.transports.Console)(),
         new (winston.transports.File)({ filename: './logs/mfa-processor.log' })
     ]
 });
 
+const now = () => { return new Date().toISOString() }
+
 let watchedDatabaseList = []
 
 process.on('unhandledRejection', (reason) => {
-    logger.log('debug', 'DEBUG: Unhandled Rejection Reason: ' + reason);
+    logger.log('debug', '[%s] DEBUG: Unhandled Rejection Reason: %s', now(), reason);
 });
 
 // Get the collection of databases to watch
 let completedDatabase = new CouchService('completed-visits')
 completedDatabase.getUserDatabaseList()
     .then(list => { start(list) })
-    .catch(err => logger.log('error', 'Error: Unable to fetch list of user databases', err))
+    .catch(err => logger.log('error', '[%s] Error: Unable to fetch list of user databases: %s', now(), err))
 
 // Store userdb instances in collection e.g. watchedDataBaseList[ 'userdb-xxxxxx', ... ]
 const start = (watchList) => {
     watchList.forEach(d => { addWatchToDatabase(d) })
     watchForNewUsers()
-    logger.log('info', 'MFA Processing Service Running...')
+    logger.log('info', '[%s] MFA Processing Service Running...', now())
 }
 
 const addWatchToDatabase = (d) => {
     watchedDatabaseList[d] = new CouchService(d)
     watchedDatabaseList[d].subscribe(processChange, generalError)
-    logger.log('info', 'Subscribed to %s', d)
+    logger.log('info', '[%s] Subscribed to %s', now(), d)
 }
 
 // Add newly created user dbs to the watch list
@@ -48,7 +50,7 @@ const watchForNewUsers = () => {
             }
         })
     }
-    const error = (err) => { logger.log('error', 'Error: Could not watch _users', JSON.stringify(err)) }
+    const error = (err) => { logger.log('error', '[%s] Error: Could not watch _users: %s', now(), JSON.stringify(err)) }
     const users = new CouchService('_users')
     users.subscribe(success, error, 'admin')
 }
@@ -78,27 +80,27 @@ const testForCompleted = (doc, db) => {
 // Move record into completed queue
 const moveRecord = (doc, db) => {
     const success = (doc) => {
-        // logger.log('info', 'DEBUG: Added doc to completed-visits', doc)
+        logger.log('debug', '[%s] DEBUG: Added doc to completed-visits: %s', now(), doc)      //
         removeIfNoError(doc.id, db)
     }
-    const error = (err) => { logger.log('error', 'Error: Completed record could not be added: ', doc._id, ' : ', JSON.stringify(err)) }
+    const error = (err) => { logger.log(now(), 'error', 'Error: Completed record could not be added: ', doc._id, ' : ', JSON.stringify(err)) }
     completedDatabase.add(doc, success, error)
 }
 
 // Ensure that record exists in completed database before removing
 const removeIfNoError = (id, db) => {
     const success = (doc) => {
-        // logger.log('info', 'DEBUG: Fetched doc from completed-visits', doc)
+        logger.log('debug', '[%s] DEBUG: Fetched doc from completed-visits: %s', now(), doc)     //
         remove(id, db)
     }
-    const error = (err) => { logger.log('error', 'Error: Completed record could not be found: ', doc._id, ' : ', JSON.stringify(err)) }
+    const error = (err) => { logger.log('error', '[%s] Error: Completed record [%s] could not be found: %s', now(), doc._id, JSON.stringify(err)) }
     completedDatabase.fetch(id, success, error)
 }
 
 const remove = (id, db) => {
-    const success = (result) => { logger.log('info', 'Assessment ' + id + ' was completed at ' + new Date().toISOString()) }
-    const error = (err) => { logger.log('error', 'Error: Completed record', id, 'could not be removed from', db, JSON.stringify(err)) }
+    const success = (result) => { logger.log('info', '[%s] Assessment [%s] was completed', now(), id) }
+    const error = (err) => { logger.log('error', '[%s] Error: Completed record [%s] could not be removed from %s: %s', now(), id, db, JSON.stringify(err)) }
     watchedDatabaseList[db].remove(id, success, error)
 }
 
-const generalError = (err) => { logger.log('error', err) }
+const generalError = (err) => { logger.log('[%s] Error: %s', now(), err) }
